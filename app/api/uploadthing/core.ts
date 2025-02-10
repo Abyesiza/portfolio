3c18e1fbb384f403f161d106146c68a870470e3e
Abyesiza <79799754+Abyesiza@users.noreply.github.com>
2025-02-10 09:43:16 +0300

    fixing more errors

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,30 +1,13 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { createUploadthing } from "uploadthing/next";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: "4MB",
-      maxFileCount: 1,
-    },
-  })
-    // Set permissions and file types for this FileRoute
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
     .middleware(() => {
       return { userId: "123" };
     })
     .onUploadComplete(() => {
       console.log("Upload complete");
     }),
-} satisfies FileRouter;
-
-export type OurFileRouter = typeof ourFileRouter;
+}; 
